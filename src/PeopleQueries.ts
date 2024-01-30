e34338d04e14f8a5159f1c3ac74e82d15694ab3b
Miguel (Yáñez-)Barreto <ma_barreto@apple.com>
2024-01-30 18:05:06 -0500

    pagination

--- src/PeopleQueries.ts
+++ src/PeopleQueries.ts
@@ -1,12 +1,21 @@
 
 function test() {
-	var allPeople = People.People!.Connections!.list(
-		'people/me',
-		{
-			pageSize: 1000,
-			personFields: 'names,birthdays'
-		}).connections!
+	let nextPageToken: string | undefined = undefined
+	let allPeople: GoogleAppsScript.People.Schema.Person[] = []
+	while (nextPageToken == undefined) {
+		var response = People.People!.Connections!.list(
+			'people/me',
+			{
+				nextPageToken: nextPageToken,
+				pageSize: 1000,
+				personFields: 'names,birthdays'
+			})
+		nextPageToken = response.nextPageToken
+		allPeople = allPeople.concat(response.connections!)
+	}
 	var peopleWithBirthdays = allPeople.filter((person) => person.birthdays != undefined)
+
+
 	console.log('Total contacts: %s / Contacts with birthdays: %s', allPeople.length, peopleWithBirthdays.length);
 
 }
